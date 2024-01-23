1475f0ea059788b048a1c52b1925d8ab2c75c582
Elisey <58297256+Elisey-e@users.noreply.github.com>
2024-01-23 23:51:42 +0300

    Added ssd1306 display support

--- curva.ino
+++ curva.ino
@@ -2,6 +2,8 @@
 #include <Wire.h>
 #include <WiFi.h>
 #include <WebServer.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include "config.h"
 #include "senseair_s8.h"
 #include "DHT.h"
@@ -10,9 +12,18 @@
 //#define DHTTYPE DHT11   // DHT 11
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 #define DHTTYPE DHT22  // DHT 22 (AM2302), AM2321
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino resetpin)
+
+#define I2C_SDA_COMMON 19
+#define I2C_SCL_COMMON 18
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
  
-const char* ssid = "MTS_GPON_E80C";   // Вводим сюда SSID
-const char* password = "Tc7ku4cT";  //Вводим пароль
+const char* ssid = "***";   // Вводим сюда SSID
+const char* password = "***";  //Вводим пароль
  
 WebServer server(80);
  
@@ -55,16 +66,59 @@ void setup() {
   server.begin();
  
   Serial.println("HTTP server started");
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+  }
+
+  display.clearDisplay();
 }
  
 void loop() {
-  server.handleClient();
-}
- 
-void handle_OnConnect() {
   Temperature = dht.readTemperature(); // Получает значение температуры
   Humidity = dht.readHumidity(); // Получаем показания влажности
   co2_requestValueAndStatus();
+  
+  server.handleClient();
+  
+  display.clearDisplay();
+  // Display Text
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  
+  display.setCursor(0, 8);
+  display.print("Temp: ");
+  display.setCursor(40, 4);
+  display.setTextSize(2);
+  display.print((int) Temperature);
+  display.print(".0");
+  display.setTextSize(1);
+  display.write(9);
+  display.setTextSize(2);
+  display.print("C\n");
+
+  display.setTextSize(1);
+  display.setCursor(0, 28);
+  display.print("Humd: ");
+  display.setCursor(40, 24);
+  display.setTextSize(2);
+  display.print((int) Humidity);
+  display.print(".0%\n");
+
+  display.setTextSize(1);
+  display.setCursor(0, 48);
+  display.print("CO2 : ");
+  display.setCursor(40, 44);
+  display.setTextSize(2);
+  display.print((int) co2_value);
+  display.setTextSize(1);
+  display.print("ppm");
+  
+  display.display();
+  delay(2000);
+}
+ 
+void handle_OnConnect() {
   server.send(200, "text/html", SendHTML(Temperature, Humidity, co2_value));
 }
  
